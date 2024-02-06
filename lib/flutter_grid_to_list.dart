450e94f453fbcfdfe46e931dad8c67e421d7fb44
mike doornenbal <mikedoornenbal9@icloud.com>
2024-02-06 16:58:14 +0100

    fix: added ci and linter

--- lib/flutter_grid_to_list.dart
+++ lib/flutter_grid_to_list.dart
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2022 Iconica
 //
 // SPDX-License-Identifier: BSD-3-Clause
-
+///
 library flutter_grid_to_list;
 
 export 'src/animated_grid_to_list.dart';
--- lib/src/animated_grid_to_list_type.dart
+++ lib/src/animated_grid_to_list_type.dart
@@ -5,18 +5,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_grid_to_list/src/animated_grid_to_list_item_builder.dart';
 
-/// An enum to which offers the possibilties the [AnimatedGridToList] can be rendered in.
+/// An enum to which offers the possibilties the [AnimatedGridToList] can be
+/// rendered in.
 enum GridToListType {
-  /// [grid] type of [GridToListType] which build a grid for [AnimatedGridToList]
+  /// [grid] type of [GridToListType] which build a grid for
+  /// [AnimatedGridToList]
   grid,
 
-  /// [list] type of [GridToListType] which build a grid for [AnimatedGridToList]
+  /// [list] type of [GridToListType] which build a grid for
+  /// [AnimatedGridToList]
   list,
 }
 
 extension NextType on GridToListType {
-  /// Extenstion method on [GridToListType] which can be called to return the next value.
-  get next =>
+  /// Extenstion method on [GridToListType] which can be called to return
+  /// the next value.
+  GridToListType get next =>
       this == GridToListType.grid ? GridToListType.list : GridToListType.grid;
 }
 
@@ -31,53 +35,53 @@ extension Render on GridToListType {
     required Function(int)? onTap,
     required BuildContext context,
     required bool isAnimating,
-  }) {
-    return !isAnimating
-        ? Wrap(
-            alignment: itemBuilder.wrapAlignment,
-            spacing: spacing,
-            children: [
-              for (int i = 0; i < itemBuilder.itemCount; i++) ...[
-                Opacity(
-                  opacity: isAnimating && tappedItem == i
-                      ? 1
-                      : isAnimating && tappedItem != i
-                          ? 0
-                          : 1,
-                  child: this == GridToListType.grid
-                      ? SizedBox(
-                          width: boxWidth ?? itemBuilder.gridItemSize.width,
-                          height: boxHeight ?? itemBuilder.gridItemSize.height,
-                          child: GestureDetector(
-                            onTap: () => onTap?.call(i),
-                            child: itemBuilder.gridItemBuilder(context, i),
+  }) =>
+      !isAnimating
+          ? Wrap(
+              alignment: itemBuilder.wrapAlignment,
+              spacing: spacing,
+              children: [
+                for (int i = 0; i < itemBuilder.itemCount; i++) ...[
+                  Opacity(
+                    opacity: isAnimating && tappedItem == i
+                        ? 1
+                        : isAnimating && tappedItem != i
+                            ? 0
+                            : 1,
+                    child: this == GridToListType.grid
+                        ? SizedBox(
+                            width: boxWidth ?? itemBuilder.gridItemSize.width,
+                            height:
+                                boxHeight ?? itemBuilder.gridItemSize.height,
+                            child: GestureDetector(
+                              onTap: () => onTap?.call(i),
+                              child: itemBuilder.gridItemBuilder(context, i),
+                            ),
+                          )
+                        : SizedBox(
+                            width: boxWidth ?? itemBuilder.listItemSize.width,
+                            height: null,
+                            child: GestureDetector(
+                              onTap: () => onTap?.call(i),
+                              child: itemBuilder.listItemBuilder(context, i),
+                            ),
                           ),
-                        )
-                      : SizedBox(
-                          width: boxWidth ?? itemBuilder.listItemSize.width,
-                          height: null,
-                          child: GestureDetector(
-                            onTap: () => onTap?.call(i),
-                            child: itemBuilder.listItemBuilder(context, i),
-                          ),
-                        ),
-                ),
+                  ),
+                ],
               ],
-            ],
-          )
-        : Align(
-            alignment: tappedItem != itemBuilder.itemCount - 1
-                ? Alignment.topCenter
-                : Alignment.bottomCenter,
-            child: SizedBox(
-              width: boxWidth ?? itemBuilder.listItemSize.width,
-              height: boxHeight ?? itemBuilder.listItemSize.height,
-              child: GestureDetector(
-                child: itemBuilder.animatedItemBuilder != null
-                    ? itemBuilder.animatedItemBuilder!(context, tappedItem!)
-                    : itemBuilder.listItemBuilder(context, tappedItem!),
+            )
+          : Align(
+              alignment: tappedItem != itemBuilder.itemCount - 1
+                  ? Alignment.topCenter
+                  : Alignment.bottomCenter,
+              child: SizedBox(
+                width: boxWidth ?? itemBuilder.listItemSize.width,
+                height: boxHeight ?? itemBuilder.listItemSize.height,
+                child: GestureDetector(
+                  child: itemBuilder.animatedItemBuilder != null
+                      ? itemBuilder.animatedItemBuilder!(context, tappedItem!)
+                      : itemBuilder.listItemBuilder(context, tappedItem!),
+                ),
               ),
-            ),
-          );
-  }
+            );
 }
